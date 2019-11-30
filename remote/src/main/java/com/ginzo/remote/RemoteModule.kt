886f6efea6c2c2831a79baee867be697869f3b40
Gerard <gserrano@gyngi.com>
2019-11-30 15:09:05 +0100

    Import remote module

---
+++ remote/src/main/java/com/ginzo/remote/RemoteModule.kt
@@ -0,0 +1,37 @@
+package com.ginzo.remote
+
+import dagger.Module
+import dagger.Provides
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+abstract class RemoteModule {
+
+  @Module
+  companion object {
+    @Provides
+    @JvmStatic
+    @Singleton
+    fun okHttpClientProvider(): OkHttpClient {
+      return OkHttpClient().newBuilder().build()
+    }
+
+    @Provides
+    @JvmStatic
+    @Singleton
+    fun retrofitProvider(okHttpClient: OkHttpClient): Retrofit {
+      return Retrofit.Builder()
+        .baseUrl("https://pizzaplaces.free.beeceptor.com")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+        .client(okHttpClient)
+        .build()
+    }
+  }
+
+}
